279e311980841649e6b3b9a3ae84b78e4b52778d
Zack <zcook04@gmail.com>
2020-09-05 21:34:39 -0400

    add middleware to check if header token valid

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+module.exports = function(req, res, next) {
+    // GET TOKEN FROM HEADER -------------------------------------------
+    const token = req.header('x-auth-token')
+
+    // CHECK IF THE TOKEN DOES NOT EXIST -------------------------------
+    if(!token) {
+        return res.status(401).json({ msg: 'No token, authorization denied'})
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'))
+
+        req.user = decoded.user
+        next()
+    } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid.'})
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+const config = require('config')
+
 
 const User = require('../models/User')
 
@@ -35,8 +38,20 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt)
 
         await user.save()
+        console.log(user)
+        
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
-        res.send('User Saved.')
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({token})
+        })
     }
     catch (err) {
         console.error(err.message)
