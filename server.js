aff9d21f7bb8417d8c356021b1f3c0374caf4b4a
Zack <zcook04@gmail.com>
2020-09-12 16:17:10 -0400

    update server.js

--- server.js
+++ server.js
@@ -28,13 +28,11 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contribute/', require('./routes/contribute'))
 app.use('/api/exams/', require('./routes/exams'))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000;
 
