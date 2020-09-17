e42f2e101e5b2ec4ea3b814a229f7bf1ea8b0863
Zack <zcook04@gmail.com>
2020-09-17 10:46:12 -0400

    refactor route functions into controller files, remove console.log

--- routes/users.js
+++ routes/users.js
@@ -20,8 +20,6 @@ router.post('/', [
         console.log(errors)
         return res.status(400).json({ errors: errors.array() })
     }
-    console.log('before')
-    console.log(registerUser(req, res))
 })
 
 module.exports = router
